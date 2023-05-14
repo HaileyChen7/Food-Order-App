092eb6ea54715ce2242e27d8b473b6067e144cd6
Hailey Chen <wche817@aucklanduni.ac.nz>
2023-05-15 00:11:05 +1200

    Initial commit

---
+++ src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import classes from "./Input.module.css";
+import Card from "./Card";
+
+const Input = () => {
+  <Card>
+    <Form>
+      <div className={classes.input}>
+        <label></label>
+        <input></input>
+      </div>
+    </Form>
+  </Card>;
+};
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,14 @@
+import classes from "./Modal.module.css";
+import Card from "./Card";
+
+const Modal = () => {
+  return (
+    <div>
+      <div>
+        <Card className={classes.modal}>
+                  
+        </Card>
+      </div>
+    </div>
+  );
+};
