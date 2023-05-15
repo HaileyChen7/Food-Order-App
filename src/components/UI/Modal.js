982ab2de50629e2e9ee283a9738e21a07dd94b68
Hailey Chen <wche817@aucklanduni.ac.nz>
2023-05-15 16:41:56 +1200

    add a modal via a react portal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,14 +1,26 @@
 import classes from "./Modal.module.css";
-import Card from "./Card";
+import ReactDOM from "react-dom";
 
-const Modal = () => {
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
   return (
-    <div>
-      <div>
-        <Card className={classes.modal}>
-                  
-        </Card>
-      </div>
+    <div className={classes.modal}>
+      <div>{props.children}</div>
     </div>
   );
 };
+
+const Modal = (props) => {
+  const partalElement = document.getElementById("Overlays");
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, partalElement)}
+      {ReactDOM.createPortal(<ModalOverlay>{ props.children}</ModalOverlay>, partalElement)}
+    </>
+  );
+};
+
+export default Modal;
